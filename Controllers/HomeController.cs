63e20628cb02ca32b9cae08cc68b82952b8863c3
Anwesha Dutta <anweshadutta891@gmail.com>
2024-03-10 15:41:06 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RajorViewEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+    }
+}
